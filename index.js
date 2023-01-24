38fc7ce2bd3cc09e0569fd8eae827232ea2b2e8c
Faddy Michel Samaan <faddy.michel@gmail.com>
2023-01-24 07:25:36 -0500

    @faddymichel: Fixed how scenario location is constructed

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ scenario = scenario ?.[ 0 ] ?.[ $ .scenario ] || scenario;
 const { scenarist, script, setting, history, binder } = this;
 const location = history [ $ .location ];
 let [ direction ] = scenario;
+let $direction;
 
 if ( direction === Symbol .for ( 'scenarist/details' ) )
 return { scenarist, script, setting, binder, scenario, location };
@@ -39,8 +40,8 @@ return setting [ scenario .shift () ] .call ( scenarist, ... scenario );
 else if ( typeof script === 'function' )
 return script .call ( binder .script, ... scenario );
 
-else if ( typeof script ?.[ direction = '$' + scenario .shift () ] !== 'undefined' )// || Object .getOwnPropertyDescriptor ( script, direction ) )
-conflict = scenario .conflict = script [ direction ];
+else if ( typeof script ?.[ $direction = '$' + scenario .shift () ] !== 'undefined' )
+conflict = scenario .conflict = script [ $direction ];
 
 else
 throw Object .assign ( Error ( 'Unknown direction' ), {
@@ -59,8 +60,7 @@ return ( history [ conflict ] || ( history [ conflict ] = Scenarist ( {
 script: conflict,
 setting,
 [ $ .binder ]: { script, scenarist },
-//typeof script === 'function' ? binder : { scenarist, script },
-[ $ .location ]: [ ... location, direction ]
+[ $ .location ]: [ ... location, direction || $direction ]
 
 } ) ) ) ( ... scenario );
 
