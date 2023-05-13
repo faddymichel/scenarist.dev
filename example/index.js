c28b9ae13447d03fd5c93569cd39fb32f93da9c1
Faddy Michel Samaan <faddy.michel@gmail.com>
2023-05-12 20:14:10 -0400

    @faddymichel: Close the following issues:
    - [issue #7](https://github.com/faddymichel/scenarist/issues/7)
    - [issue #8](https://github.com/faddymichel/scenarist/issues/8)
    - [issue #9](https://github.com/faddymichel/scenarist/issues/9)
    - [issue #10](https://github.com/faddymichel/scenarist/issues/10)
    - [issue #11](https://github.com/faddymichel/scenarist/issues/11)

--- example/index.js
+++ example/index.js
@@ -1,8 +1,8 @@
 import Scenarist from 'scenarist.dev';
 import { createInterface } from 'readline';
-import Checklist from './checklist.js';
+import Notebook from './notebook.js';
 
-const play = Scenarist ( new Checklist );
+const play = Scenarist ( new Notebook );
 const cli = createInterface ( {
 
 input: process .stdin,
@@ -14,7 +14,7 @@ cli .on ( 'line', async line => {
 
 try {
 
-console .log ( play ( ... line .trim () .toLowerCase () .split ( /\s+/ ) ) );
+console .log ( play ( ... line .trim () .split ( /\s+/ ) ) );
 
 } catch ( error ) {
 
